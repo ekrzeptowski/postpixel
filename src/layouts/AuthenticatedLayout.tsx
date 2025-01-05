f989e697f6da46d3ea3104bafe5dffaeff6b9b4b
ekrzeptowski <contact@ekrzeptowski.pl>
2025-01-05 18:56:03 +0100

    Implement authentication context and layouts with login functionality

---
+++ src/layouts/AuthenticatedLayout.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router";
+
+import { supabase } from "../utils/supabase";
+
+export const AuthenticatedLayout = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let mounted = true;
+
+    const checkAuth = async () => {
+      try {
+        const {
+          data: { session },
+          error,
+        } = await supabase.auth.getSession();
+
+        if (error) throw error;
+
+        if (mounted) {
+          if (!session?.user?.id) {
+            navigate("/login");
+          }
+          setIsLoading(false);
+        }
+      } catch (error) {
+        console.error("Auth error:", error);
+        if (mounted) {
+          navigate("/login");
+          setIsLoading(false);
+        }
+      }
+    };
+
+    checkAuth();
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event) => {
+      if (event === "SIGNED_OUT") {
+        navigate("/login");
+      }
+    });
+
+    return () => {
+      mounted = false;
+      subscription.unsubscribe();
+    };
+  }, [navigate]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return <Outlet />;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.scss";
-import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+
+import AuthProvider from "./auth.tsx";
+
+import { AuthLayout } from "./layouts/AuthLayout.tsx";
+import { AuthenticatedLayout } from "./layouts/AuthenticatedLayout.tsx";
+
+import App from "./App.tsx";
+import { LoginPage } from "./pages/Login.tsx";
+import { RegisterPage } from "./pages/Register.tsx";
+
+import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Route>
+          <Route element={<AuthenticatedLayout />}>
+            <Route index path="/" element={<App />} />
+            {/* <Route path="/profile" element={<ProfilePage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
